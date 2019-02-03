8e4df3161a7de8ecf96eb1eb50d0e59305411ab9
2019-DEV-089 <navenbrec@gmail.com>
2019-02-03 13:29:04 +0100

    Inlined triggers/acts as expressions in asserts

--- LeapYearsTests/LeapYearCheckerTests.swift
+++ LeapYearsTests/LeapYearCheckerTests.swift
@@ -3,32 +3,22 @@ import XCTest
 
 class LeapYearCheckerTests: XCTestCase {
     func test_ReturnFalse_When_AYearIsDivisibleBy4000() {
-        let isLeapYear = LeapYearChecker.check(year: 8000)
-        
-        XCTAssertFalse(isLeapYear)
+        XCTAssertFalse(LeapYearChecker.check(year: 8000))
     }
     
     func test_ReturnTrue_When_AYearIsDivisibleBy400ButNotBy4000() {
-        let isLeapYear = LeapYearChecker.check(year: 2000)
-        
-        XCTAssertTrue(isLeapYear)
+        XCTAssertTrue(LeapYearChecker.check(year: 2000))
     }
     
     func test_ReturnFalse_When_AYearIsDivisibleBy100ButNotBy400() {
-        let isLeapYear = LeapYearChecker.check(year: 1900)
-        
-        XCTAssertFalse(isLeapYear)
+        XCTAssertFalse(LeapYearChecker.check(year: 1900))
     }
     
     func test_ReturnTrue_When_AYearIsDivisibleBy4ButNotBy100() {
-        let isLeapYear = LeapYearChecker.check(year: 2012)
-        
-        XCTAssertTrue(isLeapYear)
+        XCTAssertTrue(LeapYearChecker.check(year: 2012))
     }
     
     func test_ReturnFalse_When_AYearIsNotDivisibleBy4() {
-        let isLeapYear = LeapYearChecker.check(year: 2017)
-        
-        XCTAssertFalse(isLeapYear)
+        XCTAssertFalse(LeapYearChecker.check(year: 2017))
     }
 }
