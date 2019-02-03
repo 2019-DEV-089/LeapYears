fbf8efb561d5f47219ddb2023ec540609c4c2f91
2019-DEV-089 <navenbrec@gmail.com>
2019-02-03 13:25:15 +0100

    Extracted isYearDivisbleBy400ButNotBy4000 and isYearDivisbleBy4ButNotBy100 methods

--- LeapYears/LeapYearChecker.swift
+++ LeapYears/LeapYearChecker.swift
@@ -2,10 +2,18 @@ import Foundation
 
 class LeapYearChecker {
     static func check(year: Int) -> Bool {
-        return (!isYear(year, divisibleBy: 4000) && isYear(year, divisibleBy: 400)) || (isYear(year, divisibleBy: 4) && !isYear(year, divisibleBy: 100))
+        return isYearDivisbleBy400ButNotBy4000(year) || isYearDivisbleBy4ButNotBy100(year)
     }
     
-    static func isYear(_ year: Int, divisibleBy divisor: Int) -> Bool {
+    private static func isYearDivisbleBy400ButNotBy4000(_ year: Int) -> Bool {
+        return isYear(year, divisibleBy: 400) && !isYear(year, divisibleBy: 4000)
+    }
+    
+    private static func isYearDivisbleBy4ButNotBy100(_ year: Int) -> Bool {
+        return isYear(year, divisibleBy: 4) && !isYear(year, divisibleBy: 100)
+    }
+    
+    private static func isYear(_ year: Int, divisibleBy divisor: Int) -> Bool {
         return year % divisor == 0
     }
 }
